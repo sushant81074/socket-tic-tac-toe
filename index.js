daba5115ee271ed6a2517baa261f505efc10afb9
sushantsharma-coco <cocosingh456@gmail.com>
2024-07-30 14:21:37 +0530

    done it

--- index.js
+++ index.js
@@ -14,31 +14,24 @@ const signs = {
 let currentPlayer = "x";
 let board = ["", "", "", "", "", "", "", "", ""];
 
+let winningConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 function checkWinner(socketId) {
-  let winnerCounter = 0;
-  let winnerSocketId = "";
-
-  for (let i = 0; i < 3; i++) {
-    if (((board[i] == board[i + 1]) == board[i + 2]) == socketId) {
-      winnerCounter++;
-    } else if (((board[i] == board[i + 3]) == board[i + 6]) == socketId) {
-      winnerCounter++;
-    } else if (((board[i] == board[i + 4]) == board[i + 8]) == socketId) {
-      winnerCounter++;
-    } else if (((board[i + 2] == board[i + 4]) == board[i + 6]) == socketId) {
-      winnerCounter++;
+  winningConditions.forEach(([a, b, c]) => {
+    if (((board[a] == board[b]) == board[c]) == socketId) {
+      return true;
     }
-
-    if (winnerCounter >= 1) {
-      winnerSocketId = socketId;
-    }
-  }
-
-  if (winnerSocketId.length) {
-    return true;
-  } else {
     return false;
-  }
+  });
 }
 
 try {
