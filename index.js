1c78ea76cad692ead6c4cbc3e16ed20af78b362a
sushantsharma-coco <cocosingh456@gmail.com>
2024-07-30 11:51:26 +0530

    working with chances

--- index.js
+++ index.js
@@ -11,8 +11,35 @@ const signs = {
   x: false,
   o: false,
 };
-const currentPlayer = "x";
+let currentPlayer = "x";
 let board = ["", "", "", "", "", "", "", "", ""];
+
+function checkWinner(socketId) {
+  let winnerCounter = 0;
+  let winnerSocketId = "";
+
+  for (let i = 0; i < 3; i++) {
+    if (((board[i] == board[i + 1]) == board[i + 2]) == socketId) {
+      winnerCounter++;
+    } else if (((board[i] == board[i + 3]) == board[i + 6]) == socketId) {
+      winnerCounter++;
+    } else if (((board[i] == board[i + 4]) == board[i + 8]) == socketId) {
+      winnerCounter++;
+    } else if (((board[i + 2] == board[i + 4]) == board[i + 6]) == socketId) {
+      winnerCounter++;
+    }
+
+    if (winnerCounter >= 1) {
+      winnerSocketId = socketId;
+    }
+  }
+
+  if (winnerSocketId.length) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 try {
   io.on("connection", (socket) => {
@@ -26,8 +53,10 @@ try {
       socket.on("sign", (sign) => {
         if (sign == "x") {
           signs["x"] = socket.id;
-        } else {
+        } else if ("o") {
           signs["o"] = socket.id;
+        } else {
+          socket.emit("wrong-operator");
         }
 
         console.log(sign);
@@ -57,13 +86,24 @@ try {
         ) {
           io.emit(
             "game starts now",
-            `${board} choose any position which is a number`
+            `${[
+              1, 2, 3, 4, 5, 6, 7, 8, 9,
+            ]} choose any position which is a number`
           );
           console.log("game starts now");
 
           socket.on("position", (position) => {
             console.log("positions", position);
             position = parseInt(position);
+
+            // check for correct user chance
+            if (signs[currentPlayer] !== socket.id) {
+              socket.emit(
+                "wrong-player",
+                "it's not your chance wait for yours"
+              );
+              return;
+            }
 
             // Check if x and y are within bounds of the board
             if (position < 0 || position > 9) {
@@ -81,12 +121,24 @@ try {
             }
 
             // Make the move if valid
+            console.log(board[position], position, socket.id);
             board[position] = socket.id; // Use the player's id or sign (x/o)
-            io.emit("boardUpdate", board);
-
-            console.log(socket.id, board);
+            io.emit("board-update", board);
 
             // Additional game logic such as checking for a winner or switching turns
+
+            if (checkWinner(socket.id)) {
+              io.emit("winner", socket.id);
+            } else if (
+              signs[currentPlayer] == socket.id &&
+              currentPlayer == "x"
+            ) {
+              currentPlayer = "o";
+            } else {
+              currentPlayer = "x";
+            }
+
+            console.log(board);
           });
         }
       }
@@ -109,127 +161,3 @@ try {
 server.listen(5000, () => {
   console.log("server running on port : 5000");
 });
-
-// // choose op
-// // init board
-// // start game
-// // take turns
-
-// // const onlineUserSockets = { id : socket.id }
-// // const assignedOperators = {operator : socket.id}
-// // const board = [[],[],[]]
-
-// const http = require("http");
-// const { Server } = require("socket.io");
-// const app = require("express")();
-
-// const server = http.createServer(app);
-// const io = new Server(server);
-
-// const players = {};
-// const signs = { x: null, o: null };
-// let currentPlayer = "x";
-// const board = Array(9).fill(null);
-
-// io.on("connection", (socket) => {
-//   const id = socket.handshake.query.id;
-//   players[id] = socket.id;
-
-//   // Player sign assignment
-//   if (Object.keys(players).length == 1) {
-//     socket.emit("chooseSign", { message: "Choose your sign (x or o)" });
-
-//     socket.on("sign", (sign) => {
-//       if (sign === "x" && !signs.x) {
-//         signs.x = socket.id;
-//         socket.emit("signAssigned", { sign: "x" });
-//       } else if (sign === "o" && !signs.o) {
-//         signs.o = socket.id;
-//         socket.emit("signAssigned", { sign: "o" });
-//       } else {
-//         socket.emit("signError", { message: "Sign already taken or invalid" });
-//       }
-//     });
-//   } else {
-//     if (!signs.x) {
-//       signs.x = socket.id;
-//       socket.emit("signAssigned", { sign: "x" });
-//     } else if (!signs.o) {
-//       signs.o = socket.id;
-//       socket.emit("signAssigned", { sign: "o" });
-//     } else {
-//       socket.emit("signError", { message: "Game is full" });
-//     }
-//   }
-
-//   // Start game check
-//   socket.on("start", () => {
-//     if (signs.x && signs.o) {
-//       io.emit("gameStart", { message: "Game starts now", board });
-//     }
-//   });
-
-//   // Handle move
-//   socket.on("move", (position) => {
-//     if (socket.id !== signs[currentPlayer]) {
-//       socket.emit("turnError", { message: "It's not your turn" });
-//       return;
-//     }
-
-//     if (position < 0 || position > 8 || board[position] !== null) {
-//       socket.emit("moveError", { message: "Invalid move" });
-//       return;
-//     }
-
-//     board[position] = currentPlayer;
-//     io.emit("boardUpdate", { board });
-
-//     if (checkWinner(currentPlayer)) {
-//       io.emit("gameEnd", { message: `${currentPlayer.toUpperCase()} wins!` });
-//       resetGame();
-//     } else if (board.every((cell) => cell !== null)) {
-//       io.emit("gameEnd", { message: "It's a draw!" });
-//       resetGame();
-//     } else {
-//       currentPlayer = currentPlayer === "x" ? "o" : "x";
-//       io.emit("turnUpdate", { currentPlayer });
-//     }
-//   });
-
-//   // Handle disconnect
-//   socket.on("disconnect", () => {
-//     io.emit("playerDisconnect", `${socket.id} disconnected`);
-//     if (signs.x === socket.id) signs.x = null;
-//     if (signs.o === socket.id) signs.o = null;
-//     delete players[id];
-//     resetGame();
-//   });
-// });
-
-// function checkWinner(player) {
-//   const winningCombinations = [
-//     [0, 1, 2],
-//     [3, 4, 5],
-//     [6, 7, 8],
-//     [0, 3, 6],
-//     [1, 4, 7],
-//     [2, 5, 8],
-//     [0, 4, 8],
-//     [2, 4, 6],
-//   ];
-
-//   return winningCombinations.some((combination) => {
-//     return combination.every((index) => board[index] === player);
-//   });
-// }
-
-// function resetGame() {
-//   board.fill(null);
-//   currentPlayer = "x";
-//   io.emit("boardUpdate", { board });
-//   io.emit("turnUpdate", { currentPlayer });
-// }
-
-// server.listen(5000, () => {
-//   console.log("Server running on port 5000");
-// });
